0e8efebb789471893e180d9b68e3040c943b286c
RiteshChepuri <riteshchepuri0@gmail.com>
2024-07-26 12:51:50 +0530

    feat: new Formatter

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # notion-pushing-data
+
 Pushing data into notion database\
 Get Secret Key (Notion Integration key) from the notion\
 Get databse ID from the page
--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-import { Client } from '@notionhq/client'
+import { Client } from "@notionhq/client";
 
-const NOTION_KEY = ""
+const NOTION_KEY = "";
 
-const notion = new Client({ auth: NOTION_KEY })
+const notion = new Client({ auth: NOTION_KEY });
 
-const databaseId = ""
+const databaseId = "";
 
-let service = "Facebook"
+let service = "Facebook";
 
-let mail = "abc@outlook.com"
+let mail = "abc@outlook.com";
 
-let password = ""
+let password = "";
 
 async function addItem(service, mail, password) {
-    try {
-        const response = await notion.pages.create({
-            parent: { database_id: databaseId },
-            properties: {
-                "Service": {
-                    title: [{
-                        "text": {
-                            "content": service
-                        }
-                    }]
-                },
-                "Email": {
-                    rich_text: [{
-                        "text": {
-                            "content": mail
-                        }
-                    }]
-                },
-                "Password": {
-                    rich_text: [{
-                        "text": {
-                            "content": password
-                        }
-                    }]
-                }
-            }
-        })
-        console.log(response)
-        console.log("Success")
-    } catch (error) {
-        console.log(error.body)
-    }
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        Service: {
+          title: [
+            {
+              text: {
+                content: service,
+              },
+            },
+          ],
+        },
+        Email: {
+          rich_text: [
+            {
+              text: {
+                content: mail,
+              },
+            },
+          ],
+        },
+        Password: {
+          rich_text: [
+            {
+              text: {
+                content: password,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success");
+  } catch (error) {
+    console.log(error.body);
+  }
 }
 
-addItem(service, mail, password)
+addItem(service, mail, password);
+
